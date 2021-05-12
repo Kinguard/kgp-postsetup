b84963df20bb5599e1c365930e13d08c32104dbb
Tor Krill <tor@openproducts.se>
2021-05-12 15:41:14 +0200

    * Add shell and path to cron-job * Don't fail stopping NGINX if already masked
    Signed-off-by: Tor Krill <tor@openproducts.se>

--- debian/opi-postsetup.cron.d
+++ debian/opi-postsetup.cron.d
@@ -1,4 +1,8 @@
 #
 # Run firstboot scripts only at reboot
 #
+
+SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
 @reboot root /usr/share/opi-postsetup/kgp-postsetup
